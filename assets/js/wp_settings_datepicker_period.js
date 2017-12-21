5a78624105c1422aafbea24e341b93981c92ffa6
Christoph Bessei <chris@bessei-it.eu>
2017-12-21 15:51:58 +0100

    * Refactor code * Improve localization handling

--- assets/js/wp_settings_datepicker_period.js
+++ assets/js/wp_settings_datepicker_period.js
@@ -2,39 +2,31 @@
  * @author Christoph Bessei
  */
 jQuery(document).ready(function () {
-
-  jQuery('.wp_settings_datepicker_period_start').datepicker({
+  var commonOptions = {
     // Show the 'close' and 'today' buttons
-    showButtonPanel: true,
-    closeText: objectL10n.closeText,
-    currentText: objectL10n.currentText,
-    monthNames: objectL10n.monthNames,
-    monthNamesShort: objectL10n.monthNamesShort,
-    dayNames: objectL10n.dayNames,
-    dayNamesShort: objectL10n.dayNamesShort,
-    dayNamesMin: objectL10n.dayNamesMin,
-    dateFormat: objectL10n.dateFormat,
-    firstDay: objectL10n.firstDay,
-    isRTL: objectL10n.isRTL,
+    showButtonPanel: true
+  };
+
+  if (objectL10n) {
+    jQuery.extend(commonOptions, objectL10n);
+  }
+
+  var startOptions = {
     onClose: function (selectedDate) {
       jQuery('.wp_settings_datepicker_period_end').datepicker('option', 'minDate', selectedDate);
     }
-  });
-  jQuery('.wp_settings_datepicker_period_end').datepicker({
-    // Show the 'close' and 'today' buttons
-    showButtonPanel: true,
-    closeText: objectL10n.closeText,
-    currentText: objectL10n.currentText,
-    monthNames: objectL10n.monthNames,
-    monthNamesShort: objectL10n.monthNamesShort,
-    dayNames: objectL10n.dayNames,
-    dayNamesShort: objectL10n.dayNamesShort,
-    dayNamesMin: objectL10n.dayNamesMin,
-    dateFormat: objectL10n.dateFormat,
-    firstDay: objectL10n.firstDay,
-    isRTL: objectL10n.isRTL,
+  };
+
+  var endOptions = {
     onClose: function (selectedDate) {
       jQuery('.wp_settings_datepicker_period_start').datepicker('option', 'maxDate', selectedDate);
     }
-  });
+  };
+
+  startOptions = jQuery.extend({}, commonOptions, startOptions);
+  endOptions = jQuery.extend({}, commonOptions, endOptions);
+
+  jQuery('.wp_settings_datepicker_period_start').datepicker(startOptions);
+
+  jQuery('.wp_settings_datepicker_period_end').datepicker(endOptions);
 });
